29324fc4924039ef103861c0ec484382a51b2b49
ShanchaiZ <shanchai.zahid@gmail.com>
2022-03-26 19:30:26 -0400

    added responsive homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from "react";
+import image from "../banner.jpg";
 
 export default function Home (){
-    return <h1>Testing: Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="blue banner" className="absolute object-cover w-full h-full"/>
+                <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                    <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> Hello. I'm Shanchai!</h1>
+                </section>
+        </main>
+    )
+    
+
 };
