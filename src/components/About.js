378b79f1288e350f48194e183e67e730d8763990
ShanchaiZ <shanchai.zahid@gmail.com>
2022-03-27 18:11:56 -0400

    changed alt tag on about.js

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,7 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={profilePic} alt="placeholder-profile-picture" className="absolute w-full"/>
+            <img src={profilePic} alt="profile" className="absolute w-full"/>
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
