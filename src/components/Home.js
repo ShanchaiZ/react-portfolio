df0fbaa067d6343188bb564251532990763e6ce2
ShanchaiZ <shanchai.zahid@gmail.com>
2022-03-26 20:55:53 -0400

    updated post component iin blog post, connected sanity.io to blogposts and used placeholder blogs and names

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import image from "../banner.jpg";
 export default function Home (){
     return (
         <main>
-            <img src={image} alt="blue banner" className="absolute object-cover w-full h-full"/>
+            <img src={image} alt="banner.jpg" className="absolute object-cover w-full h-full"/>
                 <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                     <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> Hello. I'm Shanchai!</h1>
                 </section>
